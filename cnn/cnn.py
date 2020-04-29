0fa8d14a8be03e9ace98d070193c0d5ec8ecc217
Harrison Huh <hhuh1@jhu.edu>
2020-04-29 10:47:05 -0400

    fixed the size

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -10,14 +10,14 @@ class Net(nn.Module):
 		self.conv1 = nn.Conv2d(1, 6, 5) # 1 in, 6 out, 5 kernel size
 		self.pool = nn.MaxPool2d(2, 2)
 		self.conv2 = nn.Conv2d(6, 16, 5)
-		self.fc1 = nn.Linear(16 * 218 * 218, 120)
+		self.fc1 = nn.Linear(16 * 109 * 109, 120)
 		self.fc2 = nn.Linear(120, 84)
 		self.fc3 = nn.Linear(84, 10)
 
 	def forward(self, x):
 		x = self.pool(F.relu(self.conv1(x)))
 		x = self.pool(F.relu(self.conv2(x)))
-		x = x.view(-1, 16 * 218 * 218)
+		x = x.view(-1, 16 * 109 * 109)
 		x = F.relu(self.fc1(x))
 		x = F.relu(self.fc2(x))
 		x = self.fc3(x)
