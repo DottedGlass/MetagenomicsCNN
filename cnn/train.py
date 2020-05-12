e0c0fb45273ef394d44c8cd680e4deb036f1efad
Victor <victor.wang647@hotmail.com>
2020-05-12 06:40:25 -0400

    formatting

--- cnn/train.py
+++ cnn/train.py
@@ -1,26 +1,12 @@
-import sys
+import argparse
 import numpy as np
 import torch
+from torch.utils import data
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 import torch.optim as optim
 from cnn import Net
 import os
-
-# variables to change
-PATH = '../data/images/read_500_error_0.01'
-TEST_SIZE = 20
-image_size = 451
-output_size = 10
-epoch_times = 4
-
-transform = transforms.Compose([transforms.ToTensor()])
-
-files = os.listdir(PATH)
-classes = [f.split('.'[0]) for f in files]
 
 # retrieve the data from each file
 # except the first TEST_SIZE, load the rest
@@ -42,29 +28,45 @@ class TrainSet(torch.utils.data.Dataset):
 			sample = self.transform(sample)
 		return sample, label
 
-trainset = TrainSet(PATH, TEST_SIZE, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+if __name__ == "__main__":
 
-net = Net(image_size, output_size)
-# define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+	# variables to change
+	PATH = '../data/images/read_500_error_0.01'
+	TEST_SIZE = 20
+	image_size = 451
+	output_size = 10
+	epoch_times = 4
 
-for epoch in range(epoch_times):
-	running_loss = 0.0
-	for i, data in enumerate(trainloader, 0):
-		inputs, labels = data
-		optimizer.zero_grad()
-		outputs = net(inputs.float())
-		loss = criterion(outputs, labels)
-		loss.backward()
-		optimizer.step()
+	transform = transforms.Compose([transforms.ToTensor()])
 
-		running_loss += loss.item()
-		if i % 2000 == 1999:
-			print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
-			running_loss = 0.0
-	torch.save(net.state_dict(), "./genomics_cnn_{}.pth".format(epoch+1))
-print('Finished training')
+	files = os.listdir(PATH)
+	classes = [f.split('.'[0]) for f in files]
 
-torch.save(net.state_dict(), "./genomics_cnn_final.pth")
+
+
+	trainset = TrainSet(PATH, TEST_SIZE, transform=transform)
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+	net = Net(image_size, output_size)
+	# define loss function and optimizer
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+	for epoch in range(epoch_times):
+		running_loss = 0.0
+		for i, data in enumerate(trainloader, 0):
+			inputs, labels = data
+			optimizer.zero_grad()
+			outputs = net(inputs.float())
+			loss = criterion(outputs, labels)
+			loss.backward()
+			optimizer.step()
+
+			running_loss += loss.item()
+			if i % 2000 == 1999:
+				print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
+				running_loss = 0.0
+		torch.save(net.state_dict(), "./genomics_cnn_{}.pth".format(epoch+1))
+	print('Finished training')
+
+	torch.save(net.state_dict(), "./genomics_cnn_final.pth")
