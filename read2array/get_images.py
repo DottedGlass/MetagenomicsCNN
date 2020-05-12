bba81bd00dd456fee026c435a6d720e268b84474
Victor <victor.wang647@hotmail.com>
2020-05-12 05:26:10 -0400

    restored python script functionality

--- read2array/get_images.py
+++ read2array/get_images.py
@@ -1,6 +1,7 @@
 """Driver script to convert all reads in reads directory into images
 
-Usage: python get_images.py ../data/long_reads/ ../data/images/
+Example of usage:
+python get_images.py ../data/long_reads/read_1000_error_1 ../data/images
 
 Reads belonging to a species are saved as 3D array.
 (read#, read_length, read_length)
@@ -19,60 +20,53 @@ from pyts.image import GramianAngularField
 from read2array import read2num
 
 
-# if __name__ == "__main__":
-#     # parse arguments
-#     parser = argparse.ArgumentParser(description='Convert reads into images')
-#     parser.add_argument('indir', type=str, help='Input dir for reads')
-#     parser.add_argument('outdir', type=str, help='Output dir for reads encoded as images')
-#     parser.add_argument('--kmer_length', default=50, help='Length of kmers used for encoding read into numbers')
-#     parser.add_argument('--array_type', default='GAF', help="Specifies type of array the read will be encoded in. 'GAF' denotes Gramian Angular Field 'MTF' denotes Markov Transition Fields")
-#     args = parser.parse_args()
-#
-#     kmer_length = int(args.kmer_length)
-#     reads_dir = args.indir
-#     images_dir = args.outdir + '/' + os.path.basename(reads_dir) + '/'
-#     if not os.path.exists(images_dir):
-#         os.makedirs(images_dir)
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Convert reads into images')
+    parser.add_argument('indir', type=str, help='Input dir for reads')
+    parser.add_argument('outdir', type=str, help='Output dir for reads encoded as images')
+    parser.add_argument('--kmer_length', default=50, help='Length of kmers used for encoding read into numbers')
+    parser.add_argument('--array_type', default='GAF', help="Specifies type of array the read will be encoded in. 'GAF' denotes Gramian Angular Field 'MTF' denotes Markov Transition Fields")
+    args = parser.parse_args()
 
-#test parameters
-kmer_length = 50
-reads_dir = '../data/long_reads/read_1000_error_1'
-images_dir = '../data/images' + '/' + os.path.basename(reads_dir) + '/'
-if not os.path.exists(images_dir):
-    os.makedirs(images_dir)
+    kmer_length = int(args.kmer_length)
+    reads_dir = args.indir
+    images_dir = args.outdir + '/' + os.path.basename(reads_dir) + '/'
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
 
-# get list of fasta files that contain the reads
-reads_files = [f for f in os.listdir(reads_dir) if f.endswith('.fa')]
+    # get list of fasta files that contain the reads
+    reads_files = [f for f in os.listdir(reads_dir) if f.endswith('.fa')]
 
-# TODO: Parallelize this for loop
-print("processing files")
-print("----------------")
-print(reads_files)
-print("----------------")
-for f in reads_files:
-    print(f)
+    # TODO: Parallelize this for loop
+    print("processing files")
+    print("----------------")
+    print(reads_files)
+    print("----------------")
+    for f in reads_files:
+        print(f)
 
-    # read in reads as list
-    bioseq_list = list(SeqIO.parse(os.path.join(reads_dir,f),"fasta"))
-    reads = np.array([str(bioseq_list[i].seq) for i in range(len(bioseq_list))])
+        # read in reads as list
+        bioseq_list = list(SeqIO.parse(os.path.join(reads_dir,f),"fasta"))
+        reads = np.array([str(bioseq_list[i].seq) for i in range(len(bioseq_list))])
 
-    # loop over all reads
-    kmer_cache = dict()
-    for i in range(len(reads)):
-        ts, kmer_cache = read2num(reads[i], kmer_length=kmer_length, kmer_cache=kmer_cache)
+        # loop over all reads
+        kmer_cache = dict()
+        for i in range(len(reads)):
+            ts, kmer_cache = read2num(reads[i], kmer_length=kmer_length, kmer_cache=kmer_cache)
 
-        # GAF conversion
-        gasf = GramianAngularField(method='summation')
-        gaf = gasf.fit_transform(ts.reshape(1,-1))
-        gaf = np.squeeze(gaf)
+            # GAF conversion
+            gasf = GramianAngularField(method='summation')
+            gaf = gasf.fit_transform(ts.reshape(1,-1))
+            gaf = np.squeeze(gaf)
 
-        # save file
-        species = f.split('.')[0]
-        gaf_path = os.path.join(images_dir, species + '_read-' + str(i) + '.npy')
-        np.save(gaf_path, gaf)
+            # save file
+            species = f.split('.')[0]
+            gaf_path = os.path.join(images_dir, species + '_read-' + str(i) + '.npy')
+            np.save(gaf_path, gaf)
 
-        if i % 5000 == 4999:
-            print("processed read", str(i+1), "in", species)
+            if i % 5000 == 4999:
+                print("processed read", str(i+1), "in", species)
 
-print('Images saved in')
-print(os.path.abspath(images_dir))
+    print('Images saved in')
+    print(os.path.abspath(images_dir))
