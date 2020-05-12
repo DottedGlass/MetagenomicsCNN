2da6107c0ae4f1e09fb0ce51f8d8d843a00c4401
Victor <victor.wang647@hotmail.com>
2020-05-12 05:47:47 -0400

    parallel processing

--- read2array/get_images.py
+++ read2array/get_images.py
@@ -14,11 +14,34 @@ This script will save the 300 images as (141,141) numpy arrays
 
 import os
 import argparse
+from joblib import Parallel, delayed
 import numpy as np
 from Bio import SeqIO
 from pyts.image import GramianAngularField
 from read2array import read2num
 
+def save_image(read_file,reads_dir,images_dir,kmer_length):
+    # read in reads as list
+    bioseq_list = list(SeqIO.parse(os.path.join(reads_dir,read_file),"fasta"))
+    reads = np.array([str(bioseq_list[i].seq) for i in range(len(bioseq_list))])
+
+    # loop over all reads
+    kmer_cache = dict()
+    for i in range(len(reads)):
+        ts, kmer_cache = read2num(reads[i], kmer_length=kmer_length, kmer_cache=kmer_cache)
+
+        # GAF conversion
+        gasf = GramianAngularField(method='summation')
+        gaf = gasf.fit_transform(ts.reshape(1,-1))
+        gaf = np.squeeze(gaf)
+
+        # save file
+        species = f.split('.')[0]
+        gaf_path = os.path.join(images_dir, species + '_read-' + str(i) + '.npy')
+        np.save(gaf_path, gaf)
+
+        if i % 5000 == 4999:
+            print("processed read", str(i+1), "in", species)
 
 if __name__ == "__main__":
     # parse arguments
@@ -26,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument('indir', type=str, help='Input dir for reads')
     parser.add_argument('outdir', type=str, help='Output dir for reads encoded as images')
     parser.add_argument('--kmer_length', default=50, help='Length of kmers used for encoding read into numbers')
-    parser.add_argument('--array_type', default='GAF', help="Specifies type of array the read will be encoded in. 'GAF' denotes Gramian Angular Field 'MTF' denotes Markov Transition Fields")
+    parser.add_argument('--cpu', default=1, help='Number of cpu cores used for parallel processing')
     args = parser.parse_args()
 
     kmer_length = int(args.kmer_length)
@@ -35,38 +58,19 @@ if __name__ == "__main__":
     if not os.path.exists(images_dir):
         os.makedirs(images_dir)
 
+    # get number of cores for parallel processing
+    num_cores = int(args.cpu)
+
     # get list of fasta files that contain the reads
     reads_files = [f for f in os.listdir(reads_dir) if f.endswith('.fa')]
 
-    # TODO: Parallelize this for loop
+    # process files
     print("processing files")
     print("----------------")
     print(reads_files)
     print("----------------")
-    for f in reads_files:
-        print(f)
 
-        # read in reads as list
-        bioseq_list = list(SeqIO.parse(os.path.join(reads_dir,f),"fasta"))
-        reads = np.array([str(bioseq_list[i].seq) for i in range(len(bioseq_list))])
-
-        # loop over all reads
-        kmer_cache = dict()
-        for i in range(len(reads)):
-            ts, kmer_cache = read2num(reads[i], kmer_length=kmer_length, kmer_cache=kmer_cache)
-
-            # GAF conversion
-            gasf = GramianAngularField(method='summation')
-            gaf = gasf.fit_transform(ts.reshape(1,-1))
-            gaf = np.squeeze(gaf)
-
-            # save file
-            species = f.split('.')[0]
-            gaf_path = os.path.join(images_dir, species + '_read-' + str(i) + '.npy')
-            np.save(gaf_path, gaf)
-
-            if i % 5000 == 4999:
-                print("processed read", str(i+1), "in", species)
+    Parallel(n_jobs=num_cores)(delayed(save_image)(f,reads_dir,images_dir,kmer_length)for f in reads_files)
 
     print('Images saved in')
     print(os.path.abspath(images_dir))
