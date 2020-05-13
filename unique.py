7167e08374a707f7a0c161caae833858b9e1ec53
Harrison Huh <hhuh@seas.upenn.edu>
2020-05-13 17:54:16 -0400

    finished

--- unique.py
+++ unique.py
@@ -3,19 +3,33 @@ import matplotlib.pyplot as plt
 
 dump_list = os.listdir('./dump')
 
-dict_list = []
+kmer_dict = dict()
 for dump in dump_list:
 	path = './dump/' + dump
+	kmer_set = set()
 	f = open(path, 'r')
-	dump_dict = dict()
-	is_kmer = False
-	count = 0
 	for line in f:
-		if not is_kmer:
-			count = int(line.strip()[1:])
-		else:
-			dump_dict[line.strip()] = count
-		is_kmer = is_kmer == False
+		if line[0] == '>':
+			continue
+		kmer_set.add(line.strip())
+	kmer_dict[dump] = kmer_set
 	f.close()
-	dict_list.append(dump_dict)
-print(dict_list)
+
+# find unique kmers for each genome
+genome_label = []
+unique_count = []
+for dump in dump_list:
+	temp = dump_list.copy()
+	temp.remove(dump)
+	temp_set = kmer_dict[dump].copy()
+	for t in temp:
+		temp_set = temp_set - kmer_dict[t]
+	dump_name = dump.split('_')
+	genome_label.append('_'.join(dump_name[:2]))
+	unique_count.append(len(temp_set))
+
+fig, axes = plt.subplots(tight_layout=True)
+bar = axes.bar(genome_label, unique_count)
+axes.set_xlabel('Genome Name')
+axes.set_ylabel('Unique 50-mer count')
+plt.savefig('figure.png')
