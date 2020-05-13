595ab3fdd97b330a9967470b70393def8d8bf2de
Victor <victor.wang647@hotmail.com>
2020-05-13 17:02:50 -0400

    loads training data from pickle

--- cnn/test.py
+++ cnn/test.py
@@ -1,5 +1,10 @@
+import os
+import pickle
+import argparse
 import numpy as np
+from sklearn.metrics import confusion_matrix
 import torch
+from torch.utils import data
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
@@ -7,48 +12,45 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import torch.optim as optim
 from cnn import Net
-import os
-from sklearn.metrics import confusion_matrix
+from dataset import Dataset
 
-PATH = '../data/images/read_500_error_0.01'
-TEST_SIZE = 20
-CNN_FILE = './genomics_cnn_final.pth'
+# variables to change
+reads_dir = '/home-4/xwang145@jhu.edu/workzfs-mschatz1/xwang145/data/long_reads/read_1000_error_1'
+cnn_dir = '/work-zfs/mschatz1/xwang145/data/cnn'
+cnn_model_file = 'cnn_epoch_0.i_109999.pth'
+kmer_length = 50
+test_percent = 0.2
+max_epochs = 100
+
+# file locations
+cnn_name = os.path.basename(reads_dir)
+cnn_dir = os.path.join(cnn_dir,cnn_name)
+cnn_model_file = os.path.join(cnn_dir,cnn_model_file)
+
+reads_files = [f for f in os.listdir(reads_dir) if f.endswith('.fa')]
+num_classes = len(reads_files)
+image_size = read_length - kmer_length + 1
+
+# retrieve testing parition and labels
+with open(os.path.join(cnn_dir,'test_list.pickle'), 'wb') as f:
+	test_list = pickle.load(f)
+with open(os.path.join(cnn_dir,'labels.pickle'), 'wb') as f:
+	labels_dict = pickle.load(f)
+
+# generators
 transform = transforms.Compose([transforms.ToTensor()])
-image_size = 451
-output_size = 10
+testset = Dataset(reads_dir, reads_files, test_list, labels_dict, kmer_length, transform=transform)
+testloader = data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-files = os.listdir(PATH)
-classes = [f.split('.'[0]) for f in files]
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+torch.backends.cudnn.benchmark = True
 
-# retrieve the data from each file
-# except the first 20, load the rest
-class TestSet(torch.utils.data.Dataset):
-	def __init__(self, filepath, test_size, transform=None):
-		self.data = []
-		self.transform = transform
-		files = os.listdir(filepath)
-		for i, f in enumerate(files):
-			dat = np.load(filepath + '/' + f)[:test_size]
-			for d in dat:
-				self.data.append( (d, i) )
-
-	def __len__(self):
-		return len(self.data)
-	def __getitem__(self, idx):
-		sample, label = self.data[idx]
-		if self.transform:
-			sample = self.transform(sample)
-		return sample, label
-
-testset = TestSet(PATH, TEST_SIZE, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
-
-net = Net(image_size, output_size)
-net.load_state_dict(torch.load(CNN_FILE))
-
-# outputs = net(images)
-# _, predicted = torch.max(outputs, 1)
-# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+# initialize CNN
+net = Net(image_size, num_classes, device)
+net.load_state_dict(torch.load(cnn_model_file))
+net.to(device)
 
 correct = 0
 total = 0
@@ -56,22 +58,26 @@ label_list = []
 label_correct = []
 predicted_list = []
 with torch.no_grad():
-	for data in testloader:
-		inputs, labels = data
-		outputs = net(inputs.float())
+	for local_data in testloader:
+		# get samples and labels
+		local_batch, local_labels = local_data
+		# Transfer to GPU
+		local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+
+		outputs = net(local_batch.float())
 		_, predicted = torch.max(outputs.data, 1)
-		total += labels.size(0)
-		correct += (predicted == labels).sum().item()
+		total += local_labels.size(0)
+		correct += (predicted == local_labels).sum().item()
 
-		label_list.append(labels)
+		label_list.append(local_labels)
 		predicted_list.append(predicted)
-		label_correct.append((predicted == labels).sum().item())
+		label_correct.append((predicted == local_labels).sum().item())
 
-print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+print('Accuracy of the network on the test reads: %d %%' % (100 * correct / total))
 
 print('Accuracy by label')
 for l,c in zip(label_list,label_correct):
-	print(l,":",str(c/TEST_SIZE))
+	print(l,":",str(c/len(test_list)))
 
 print('Confusion Matrix')
 cf_m = confusion_matrix(label_list, predicted_list)
