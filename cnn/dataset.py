e267d5fe1abbb9ff8347e13d27ac90e9983e732c
Victor <victor.wang647@hotmail.com>
2020-05-13 07:49:18 -0400

    cnn now reads in long reads and does conversion to gaf at each training step

---
+++ cnn/dataset.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import torch
+from torch.utils import data
+import torchvision.transforms as transforms
+from Bio import SeqIO
+from pyts.image import GramianAngularField
+
+class Dataset(data.Dataset):
+	def __init__(self, reads_dir, reads_files, list_IDs, labels, kmer_length, transform=None):
+		"""Initialization
+		Parameters
+	    ----------
+	    list_IDs : list
+	        list of file names and sample number specific to that file.
+			Each element in the list is a string of the form
+			reads_file:sample_num
+			e.g.
+			NC_004113.reads.1000bp.fa:0
+		kmer_length : int
+			length of kmers used for encoding the reads as time series
+		"""
+
+		self.list_IDs = list_IDs
+		self.labels = labels
+		self.kmer_length = kmer_length
+		self.transform = transform
+
+		# open long reads
+		long_reads = dict()
+		for file in reads_files:
+			print("Opening long reads file:",file)
+			bioseq_list = list(SeqIO.parse(os.path.join(reads_dir,file),"fasta"))
+			reads = np.array([str(bioseq_list[i].seq) for i in range(len(bioseq_list))])
+			long_reads[file] = reads
+		self.long_reads = long_reads
+
+	def __len__(self):
+		"""Denotes total number of samples
+		"""
+		return len(self.list_IDs)
+
+	def __getitem__(self, idx):
+		"""Generates one sample of data
+		"""
+
+		# select sample and load label
+		ID = self.list_IDs[idx]
+
+		file, sample_num = ID.split(':')
+		sample_num = int(sample_num)
+		y = self.labels[file]
+
+		# load read
+		read = self.long_reads[file][sample_num]
+
+		# convert read to time series
+		ts = self.read2ts(read,self.kmer_length)
+
+		# convert time series to GAF
+		gasf = GramianAngularField(method='summation')
+    	X = gasf.fit_transform(ts.reshape(1,-1)).squeeze()
+
+		# apply transform
+		if self.transform:
+			X = self.transform(X)
+
+		return X, y
+
+	def read2ts(self, read, kmer_length):
+	    """Converts the read into a 1D array of numbers by encoding kmer as numbers
+	    Parameters
+	    ----------
+	    read : str
+	        A read from shotgun sequencing. Bases must be A, C, G, or T.
+	    kmer_length: int
+	        Length of kmers used for encoding read into numbers
+
+	    Returns
+	    -------
+	    numpy.array
+	    """
+
+	    num_kmers = len(read) - kmer_length + 1
+	    nt2int = {'A':'0', 'C':'1', 'G':'2', 'T':'3'}
+	    time_series = []
+	    for i in range(num_kmers):
+
+	        # split into kmers
+	        kmer = read[i:i+kmer_length]
+
+	        # convert to base 4 num
+	        int_mer  = []
+	        for b in kmer:
+	            if b not in nt2int:
+	                b = 'A'         # replace unknown bases with A
+	            int_mer.append(nt2int[b])
+
+	        kmer_base4 = "".join(int_mer)
+
+	        # convert to base 10 number
+	        kmer_num = int(kmer_base4,base=4)
+
+	        time_series.append(kmer_num)
+
+	    return np.array(time_series)
--- cnn/train.py
+++ cnn/train.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 import argparse
 import numpy as np
 import torch
@@ -5,68 +7,118 @@ from torch.utils import data
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torch.optim as optim
+from pyts.image import GramianAngularField
 from cnn import Net
-import os
+from dataset import Dataset
 
-# retrieve the data from each file
-# except the first TEST_SIZE, load the rest
-class TrainSet(torch.utils.data.Dataset):
-	def __init__(self, filepath, test_size, transform=None):
-		self.data = []
-		self.transform = transform
-		files = os.listdir(filepath)
-		for i, f in enumerate(files):
-			dat = np.load(filepath + '/' + f)[test_size:]
-			for d in dat:
-				self.data.append( (d, i) )
+def main():
+		# variables to change
+		max_epochs = 100
+		reads_dir = '../data/long_reads/read_1000_error_1'
+		cnn_dir = '../data/cnn'
+		kmer_length = 50
+		test_percent = 0.2
 
-	def __len__(self):
-		return len(self.data)
-	def __getitem__(self, idx):
-		sample, label = self.data[idx]
-		if self.transform:
-			sample = self.transform(sample)
-		return sample, label
+		# prepare output folder that will contain the model and train/test split
+		cnn_name = os.path.basename(reads_dir)
+		cnn_dir = os.path.join(cnn_dir,cnn_name)
+		if not os.path.exists(cnn_dir):
+	        os.makedirs(cnn_dir)
+
+		# read parameters file to get information of dataset
+		param_file = os.path.join(reads_dir,'parameters.txt')
+		num_samples = dict()
+		with open(param_file, 'r') as f:
+			read_length = int(f.readline().rstrip().split(': ')[1])
+			error_rate = int(f.readline().rstrip().split(': ')[1])
+			coverage = int(f.readline().rstrip().split(': ')[1])
+			_ = f.readline()  	#skip this line
+			_ = f.readline()	#skip this line
+			for line in f:
+				species, count = line.split(': ')
+				num_samples[species] = int(count)
+
+		reads_files = [f for f in os.listdir(reads_dir) if f.endswith('.fa')]
+		num_classes = len(reads_files)
+		image_size = read_length - kmer_length + 1
+
+		# make training and testing partition
+		test_list = []
+		train_list = []
+		labels = dict()
+		for i,file in enumerate(reads_files):
+		    species = file.split('.')[0]
+			num  = num_samples[species]
+			test_thres = round(num*test_percent)
+
+			# add to test_list
+			for k in range(0,test_thres):
+				ID = file + ':' + str(k)
+				test_list.append(ID)
+				labels[ID] = i
+
+			# add to train_list
+			for k in range(test_thres,num):
+				ID = file + ':' + str(k)
+				train_list.append(ID)
+				labels[ID] = i
+
+		# save training and testing partition, along with labels
+		with open(os.path.join(cnn_dir,'train_list.pickle'), 'wb') as f:
+			pickle.dump(train_list, f)
+		with open(os.path.join(cnn_dir,'test_list.pickle'), 'wb') as f:
+			pickle.dump(test_list, f)
+		with open(os.path.join(cnn_dir,'labels.pickle'), 'wb') as f:
+			pickle.dump(labels, f)
+
+		# generators
+		transform = transforms.Compose([transforms.ToTensor()])
+		trainset = Dataset(reads_dir, reads_files, train_list, labels, kmer_length, transform=transform)
+		trainloader = data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+		# CUDA for PyTorch
+		use_cuda = torch.cuda.is_available()
+		device = torch.device("cuda:0" if use_cuda else "cpu")
+		cudnn.benchmark = True
+
+		# initialize CNN
+		net = Net(image_size, num_classes)
+		net.to(device)
+
+		# define loss function and optimizer
+		criterion = nn.CrossEntropyLoss()
+		optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+		# loop over epochs
+		for epoch in range(max_epochs):
+			running_loss = 0.0
+			# training
+			for i, local_data in enumerate(trainloader):
+				# get samples and labels
+				local_batch, local_labels = local_data
+				# Transfer to GPU
+		        local_batch, local_labels = local_batch.to(device), local_labels.to(device)
+
+				optimizer.zero_grad()
+				outputs = net(local_batch.float())
+				loss = criterion(outputs, local_labels)
+				loss.backward()
+				optimizer.step()
+
+				running_loss += loss.item()
+				if i % 2000 == 1999:
+					print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
+					running_loss = 0.0
+
+			# save progress after each epoch
+			cnn_save_name = os.path.join(cnn_dir, "cnn_epoch_" + str(epoch) + ".pth")
+			torch.save(net.state_dict(), cnn_save_name)
+
+
+		cnn_save_name_final = os.path.join(cnn_dir, "cnn_final.pth")
+		torch.save(net.state_dict(), cnn_save_name_final)
+		print('Finished training. Model path is')
+		print(os.path.abspath(cnn_save_name_final))
 
 if __name__ == "__main__":
-
-	# variables to change
-	PATH = '../data/images/read_500_error_0.01'
-	TEST_SIZE = 20
-	image_size = 451
-	output_size = 10
-	epoch_times = 4
-
-	transform = transforms.Compose([transforms.ToTensor()])
-
-	files = os.listdir(PATH)
-	classes = [f.split('.'[0]) for f in files]
-
-
-
-	trainset = TrainSet(PATH, TEST_SIZE, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-
-	net = Net(image_size, output_size)
-	# define loss function and optimizer
-	criterion = nn.CrossEntropyLoss()
-	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-	for epoch in range(epoch_times):
-		running_loss = 0.0
-		for i, data in enumerate(trainloader, 0):
-			inputs, labels = data
-			optimizer.zero_grad()
-			outputs = net(inputs.float())
-			loss = criterion(outputs, labels)
-			loss.backward()
-			optimizer.step()
-
-			running_loss += loss.item()
-			if i % 2000 == 1999:
-				print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
-				running_loss = 0.0
-		torch.save(net.state_dict(), "./genomics_cnn_{}.pth".format(epoch+1))
-	print('Finished training')
-
-	torch.save(net.state_dict(), "./genomics_cnn_final.pth")
+	main()
